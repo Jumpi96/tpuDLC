7d59e3e2bbdd41510d2f5f329ba7a781f2b54d96
juampilorenzo <juampilorenzo@gmail.com>
2017-05-02 11:11:39 -0300

    Borrando todo

--- SAC-2017/SAC-war/ddbb/tmp.sql
+++
@@ -1,9 +0,0 @@
-delete from calificacion;
-
-select fn_savecalificacion(null, 1, 5, '2012-09-30', 10);
-select fn_savecalificacion(null, 1, 5, '2012-10-03', 10);
-select fn_savecalificacion(null, 3, 5, '2012-10-01', 9);
-select fn_savecalificacion(null, 3, 5, '2012-10-04', 10);
-
-select * from v_calificacion;
-
